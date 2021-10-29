1b15bdf8058b87c7212b0c50bba9a93ad17a4b23
Luiggi Annes Bellincanta <65421097+luiggidev@users.noreply.github.com>
2021-10-29 06:58:08 -0300

    uninstalling global babel and live-server and replacing by a local install with scripts

--- indecision-app/public/scripts/app.js
+++ indecision-app/public/scripts/app.js
@@ -8,6 +8,10 @@ function _possibleConstructorReturn(self, call) { if (!self) { throw new Referen
 
 function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
+// Grab the add function from the add.js file in the utils folder
+// Grab React from the react npm module
+// add(2, 4)
+
 var IndecisionApp = function (_React$Component) {
   _inherits(IndecisionApp, _React$Component);
 
