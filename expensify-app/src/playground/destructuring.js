8153d45a55087a573be06bbe5d3abc453cc1b337
luiggidev <luiggibellincanta@gmail.com>
2021-11-13 17:09:37 -0300

    Testing expensify cl

--- expensify-app/src/playground/destructuring.js
+++ expensify-app/src/playground/destructuring.js
@@ -43,3 +43,5 @@ const item = ['Coffee (iced)', '$3.00', '$3.50', '$3.75'];
 const [itemName, , mediumPrice] = item;
 
 console.log(`A medium ${itemName} costs ${mediumPrice}`);
+// console.log(`A medium ${itemName} costs ${mediumPrice}`);
+// console.log(`A medium ${itemName} costs ${mediumPrice}`);
